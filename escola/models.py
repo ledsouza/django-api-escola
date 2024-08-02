9330da2309270bc313204898300e119e0e041ffe
ledsouza <leandro.souza.159@gmail.com>
2024-08-02 14:57:04 -0300

    criando models Estudante e Curso

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Estudante(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, blank=False)
+    cpf = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+    celular = models.CharField(max_length=14)
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+
+    codigo = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100, blank=False)
+    nivel = models.CharField(max_length=1, choices=NIVEL,
+                             blank=False, null=False, default='B')
+
+    def __str__(self) -> str:
+        return self.nome
